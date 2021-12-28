95aaf484e0013dc0c3547b7a523213793848b05d
Etay Rozenberg <etayro@Etays-MacBook-Pro.local>
2021-12-28 15:41:21 +0200

    trying to update repo with new files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
 import React from 'react';
+import { Routes , Route } from 'react-router-dom';
 
-import HomePage from './homepage.component';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-      Hello!
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
