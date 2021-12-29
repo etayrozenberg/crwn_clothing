58955db64022bc6487e1238d9b9f9150c3db3e50
Etay Rozenberg <etayro@Etays-MacBook-Pro.local>
2021-12-29 12:08:07 +0200

    added the button configuration for sign-in

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage/>} />
         <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/signin' element={<SignInAndSignUpPage/>} />
       </Routes>
     </div>
   );
